c4c5524f5586513cadd24882572cd1c08bae53ae
Jonathan Tan <jtsz5057@gmail.com>
2024-05-08 15:08:08 +1000

    03 calculator completed

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,23 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+    let result = 0
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1*num2;
+            break;
+        case '/':
+            result = num1/num2;
+            break;
+        default:
+            throw new Error('Invalid operator')
+    }
+    return result;
+}
 
 module.exports = calculator;
