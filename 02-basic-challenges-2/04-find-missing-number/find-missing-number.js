18de32c4950001dbdff0b678161bdbf01cff80d7
Jonathan Tan <jtsz5057@gmail.com>
2024-05-22 23:52:19 +1000

    04 find missing number completed

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,31 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+    if (!arr || arr.length === 0) {
+        return undefined;
+      }
+    
+      const n = arr.length + 1;
+      const expectedSum = (n * (n + 1)) / 2;
+    
+      let actualSum = 0;
+      for (let i = 0; i < arr.length; i++) {
+        actualSum += arr[i];
+      }
+    
+      return expectedSum - actualSum;
+    }
+    
+    // Using reduce
+    function findMissingNumber(arr) {
+      if (!arr || arr.length === 0) {
+        return undefined;
+      }
+    
+      const n = arr.length + 1;
+      const expectedSum = (n * (n + 1)) / 2;
+    
+      const actualSum = arr.reduce((sum, num) => sum + num, 0);
+    
+      return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
