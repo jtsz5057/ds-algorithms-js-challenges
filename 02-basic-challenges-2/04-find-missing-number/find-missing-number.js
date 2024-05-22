a3224556bde4513d62f11492f3344260d8bfc2a6
Jonathan Tan <jtsz5057@gmail.com>
2024-05-23 09:06:11 +1000

    remove reduce method for 04 find missing number challenge

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -13,19 +13,6 @@ function findMissingNumber(arr) {
     
       return expectedSum - actualSum;
     }
-    
-    // Using reduce
-    function findMissingNumber(arr) {
-      if (!arr || arr.length === 0) {
-        return undefined;
-      }
-    
-      const n = arr.length + 1;
-      const expectedSum = (n * (n + 1)) / 2;
-    
-      const actualSum = arr.reduce((sum, num) => sum + num, 0);
-    
-      return expectedSum - actualSum;
-}
+
 
 module.exports = findMissingNumber;
