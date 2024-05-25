6fd430c1bdef9f427fa7150f616bf08ede1736bc
Jonathan Tan <jtsz5057@gmail.com>
2024-05-26 00:21:36 +1000

    06 are all char unique completed

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,15 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+    const charSet = new Set();
+    
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        if (charSet.has(char)) {
+            return false;
+        }
+        charSet.add(char);
+    }
+    
+    return true;
+}
 
 module.exports = areAllCharactersUnique;
